8c82ff81835458d9d820afea4faf5d3db1213ce8
anwarsdath96 <anwarsadath787@gmail.com>
2026-09-14 15:27:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-// Personal OS V02 entry point
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
